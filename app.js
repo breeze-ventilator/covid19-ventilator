7fa8542a463f365eff53dfa4d093d7ac4ba03ce3
Raffi Hotter <rhotter@users.noreply.github.com>
2020-03-25 11:58:41 -0400

    moved to web app, will switch to electorn afterwards (unsure if electron works well with touch)

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log('Example app listening on port ${port}!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
