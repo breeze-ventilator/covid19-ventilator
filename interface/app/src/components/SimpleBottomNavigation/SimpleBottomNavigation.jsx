888aad6a71e693924ce0c39fbd2365868c96b285
Jasmine Wang <jasminewang76@gmail.com>
2020-03-29 19:55:53 -0400

    not working, re-render component

--- interface/app/src/components/SimpleBottomNavigation/SimpleBottomNavigation.jsx
+++ interface/app/src/components/SimpleBottomNavigation/SimpleBottomNavigation.jsx
@@ -5,37 +5,72 @@ import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import RestoreIcon from '@material-ui/icons/Restore';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
+import Vitals from '../Vitals/Vitals';
+import { Link } from 'react-router-dom';
 
-const useStyles = makeStyles({
-  root: {
-    width: 500,
-  },
-});
 
-export default function SimpleBottomNavigation() {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
+export default class SimpleBottomNavigation extends React.Component{
+  constructor(props) {
+    super(props);
 
-  return (
-    <BottomNavigation
-      value={value}
-      onChange={(event, newValue) => {
-        setValue(newValue);
-      }}
-      showLabels
-      className={classes.root}
-    >
-      <BottomNavigationAction
-        label="Parameters"
-        icon={<RestoreIcon />}
-        onPress={() => this.setState({ active: 'today' })}
+    this.state = {
+      classes: makeStyles({
+        root: {
+          width: 500,
+        },
+      }),
+      value: 0,
+      viewForm: false
+    }
+
+    this.page = null;
+  }
+
+  render() {
+    return(
+  <BottomNavigation
+        value={this.state.value}
+        onChange={(event, newValue) => {
+          this.state.value = newValue;
+        }}
+        showLabels
+        className={this.state.classes.root}
+      >
+        <BottomNavigationAction
+          label="Parameters"
+          icon={<RestoreIcon />}
+          onPress={() => this.setState(
+            { page: <Vitals timeSeriesData={this.state.data.tidalVolume} />,
+             viewForm: true
+           }
+            )}
+          />
+
+        <BottomNavigationAction
+          label="Diagnostics"
+          icon={<FavoriteIcon />}
+          onPress={() => this.setState(
+            { page: <Vitals timeSeriesData={this.state.data.tidalVolume} />,
+            viewForm: true }
+            )}
         />
-      <BottomNavigationAction
-        label="Diagnostics"
-        icon={<FavoriteIcon />} />
-      <BottomNavigationAction
-        label="Alarms"
-        icon={<LocationOnIcon />} />
-    </BottomNavigation>
-  );
+
+        <BottomNavigationAction
+          label="Alarms"
+          icon={<LocationOnIcon />}
+          onPress={() => this.setState(
+            { page: <Vitals timeSeriesData={this.state.data.tidalVolume} />,
+            viewForm: true }
+            )}
+          />
+
+        <div>
+          {(this.state.viewForm) ?
+          this.page : this.page}
+        </div>
+      </BottomNavigation>
+    );
+  }
 }
+
+
