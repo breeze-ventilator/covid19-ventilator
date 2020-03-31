371c1998906709b6f7f764d9668f174063ee11ee
merny93 <simtart@gmail.com>
2020-03-31 01:50:42 -0400

    small

--- firmware/arduino/libraries/BlowerFanServo/BlowerFanServo.cpp
+++ firmware/arduino/libraries/BlowerFanServo/BlowerFanServo.cpp
@@ -9,10 +9,11 @@ void BlowerFanServo::begin() {
   // Initialize the esc by giving 100% power wait for beep then 0% power and wait for beep
   // https://www.arduino.cc/en/Reference/ServoWrite
   // TODO: should we wait here?
-  blowerFan.write(0);
+  blowerFan.write(90);
   delay(100);
-  blowerFan.write(180);
-  delay(2000);
-  blowerFan.write(0);
-  delay(2000);
+  blowerFan.write(15);
+  delay(4000);
+  blowerFan.write(90);
+  delay(1000);
+  blowerFan.write(15);
 }
