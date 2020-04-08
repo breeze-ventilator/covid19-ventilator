f2b077e7b4f56464cdfe3cd73f945c52334b7fa6
John Fish <john@johnafish.ca>
2020-04-08 12:14:24 -0400

    Use FlexValueCard in Vitals Screen
    Exclusively use the FlexValueCard on the vitals screen

--- interface/app/src/components/Vitals/Vitals.jsx
+++ interface/app/src/components/Vitals/Vitals.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import ValueCard from '../Card/Card';
 import FlexValueCard from '../Card/FlexValueCard';
 import Grid from "@material-ui/core/Grid";
 import './css/vitals.css'
-import SmallValueCard from '../Card/SmallerValueCard';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
@@ -27,22 +25,10 @@ export default class Vitals extends React.Component {
   }
 
   render() {
-    return (
-      <div className="mainContainer">
-        {/* Header Observables */}
-        <Grid container direction="row">
-          <Grid item xs={6}>
-              <FlexValueCard value={this.state.data.tidalVolume} unit='mL' prominence="h1" name='Tidal Volume'/>
-          </Grid>
-          <Grid item xs={6}>
-              <FlexValueCard value={this.state.data.pressure} prominence="h1" unit='kPa' name='Pressure'/>
-          </Grid>
-        </Grid>
-
-        {/* TODO: Graphs go here */}
-        
-        {/* Footer modifiables */}
-        <Grid container className="bottom">
+    let footer;
+    if (this.props.allParameters.isPressureControlState) {
+      footer = ( 
+      <Grid container className="bottom">
           <Grid container direction="row">
             <Grid item xs={4}>
               <FlexValueCard value="ON" unit=" " prominence="h2" name="Pressure Control" />
@@ -51,7 +37,7 @@ export default class Vitals extends React.Component {
               <FlexValueCard value="80" unit="%" prominence="h2" name="FiO2" />
             </Grid>
             <Grid item xs={4}>
-              <FlexValueCard value="12" unit="rpm" prominence="h2" name="Respiratory Rate" />
+              <FlexValueCard value="12" unit="" prominence="h2" name="Respiratory Rate" />
             </Grid>
           </Grid>
           <Grid container direction="row">
@@ -65,96 +51,52 @@ export default class Vitals extends React.Component {
               <FlexValueCard value="20" unit="cm H2O" prominence="h2" name="Peak Pressure" />
             </Grid>
           </Grid>
+        </Grid>);
+    } else {
+      footer = (
+        <Grid container className="bottom">
+          <Grid container direction="row">
+            <Grid item xs={4}>
+              <FlexValueCard value="ON" unit=" " prominence="h2" name="Pressure Support" />
+            </Grid>
+            <Grid item xs={4}>
+              <FlexValueCard value="80" unit="%" prominence="h2" name="FiO2" />
+            </Grid>
+            <Grid item xs={4}>
+              <FlexValueCard value={this.props.allParameters.sensitivity} unit="" prominence="h2" name="Sensitivity" />
+            </Grid>
+          </Grid>
+          <Grid container direction="row">
+            <Grid item xs={4}>
+              <FlexValueCard value="5" unit="cm H2O" prominence="h2" name="PEEP" />
+            </Grid>
+            <Grid item xs={4}>
+              <FlexValueCard value="1.0" unit="s" prominence="h2" name="Apnea Time" />
+            </Grid>
+            <Grid item xs={4}>
+              <FlexValueCard value="20" unit="cm H2O" prominence="h2" name="Peak Pressure" />
+            </Grid>
+          </Grid>
         </Grid>
-      </div>
-    )}
+      )};
+ 
+    return (
+      <div className="mainContainer">
+        {/* Header Observables */}
+        <Grid container direction="row">
+          <Grid item xs={6}>
+              <FlexValueCard value={this.state.data.tidalVolume} unit='mL' prominence="h1" name='Tidal Volume'/>
+          </Grid>
+          <Grid item xs={6}>
+              <FlexValueCard value={this.state.data.pressure} prominence="h1" unit='kPa' name='Pressure'/>
+          </Grid>
+        </Grid>
+
+        {/* TODO: Graphs go here */} 
+        {/* <LineChart timeSeriesData={this.props.timeSeriesData} /> */}
+
+        {/* Footer modifiables */}
+        { footer }
+     </div>
+    )};
 }
-/*
-
-
-        <Grid
-          container
-          direction="row"
-          alignItems="center"
-          justify="center"
-        >
-          <Grid item direction="column" spacing={1} justify="space-around" alignItems="center">
-
-          <Grid item>
-
-          {this.props.allParameters.isPressureControlState
-            &&
-            <SmallValueCard value="ON" unit='' name='Pressure Control'/>
-          }
-          </Grid>
-          <Grid item>
-          {this.props.allParameters.isPressureControlState
-            &&
-            <SmallValueCard value={this.props.allParameters.respiratoryRate} unit='bpm' name='Respiratory Rate'/>
-          }
-          </Grid>
-
-          <Grid item>
-          {this.props.allParameters.isPressureControlState
-            &&
-            <SmallValueCard value={this.props.allParameters.inspiratoryTime} unit='s' name='Inspiratory Time'/>
-          }
-          </Grid>
-
-          <Grid item>
-          {!this.props.allParameters.isPressureControlState
-            &&
-            <SmallValueCard value="ON" unit='' name='Pressure Support'/>
-          }
-          </Grid>
-
-          <Grid item>
-          {!this.props.allParameters.isPressureControlState
-            &&
-            <SmallValueCard value={this.props.allParameters.sensitivity} unit='' name='Sensitivity'/>
-          }
-          </Grid>
-
-          <Grid item>
-          {!this.props.allParameters.isPressureControlState
-            &&
-            <SmallValueCard value={this.props.allParameters.apneaTime} unit='s' name='Apnea Time'/>
-          }
-          </Grid>
-          </Grid>
-
-          <Grid item direction="column" spacing={1} justify="space" alignItems="center">
-          
-            <Grid item>
-            <SmallValueCard value={this.props.allParameters.fiO2} unit='%' name='FiO2'/>
-            </Grid>
-
-            <Grid item>
-            <SmallValueCard value={this.props.allParameters.peep} unit='cmH2O' name='PEEP'/> 
-            </Grid>
-
-            <Grid item>
-            <SmallValueCard value={this.props.allParameters.peakPressure} unit='cmH2O' name='Peak Pressure'/> 
-            </Grid>
-          </Grid>
-        </Grid>
-
-        <Grid
-          container
-          direction="row"
-          alignItems="center"
-          justify="center"
-        >
-          <Button className="standByButton" variant="contained" align="center" style={{ backgroundColor: "#D9A3DE" }}>
-            <strong>
-            Turn on Standby
-            </strong>
-            
-          </Button>
-        </Grid>
-      </div>
-      // <LineChart timeSeriesData={this.props.timeSeriesData} />
-    );
-  }
-}
-*/
