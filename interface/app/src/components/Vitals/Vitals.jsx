f218b99ab090ded07d084d4b6bd42bbc7d6fb9b1
Harsh Patel <me@mrharshpatel.com>
2020-04-08 20:46:36 -0400

    Format changes

--- interface/app/src/components/Vitals/Vitals.jsx
+++ interface/app/src/components/Vitals/Vitals.jsx
@@ -4,7 +4,6 @@ import Grid from "@material-ui/core/Grid";
 import './css/vitals.css'
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import StatusBar from '../StatusBar/StatusBar';
 
 export default class Vitals extends React.Component {
   constructor(props) {
