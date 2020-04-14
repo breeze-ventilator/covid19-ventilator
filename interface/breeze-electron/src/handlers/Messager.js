d10e7244e23add7cec227eee653ddf77a34e2d0d
Raffi Hotter <rhotter@users.noreply.github.com>
2020-04-14 16:17:31 -0400

    Sample listener changes

--- interface/breeze-electron/src/handlers/Messager.js
+++ interface/breeze-electron/src/handlers/Messager.js
@@ -1,71 +1,42 @@
 import socketIOClient from "socket.io-client";
+import {readingNames, readingsInfo} from '../util/constants';
 
 export default class Messager {
-    constructor(port){
-        this.socket = socketIOClient('http://localhost:' + port);
+    constructor(port, useSampleListener=false){
+		this.useSampleListener = useSampleListener;
+		this.socket = null;
+		if(!useSampleListener){
+			this.socket = socketIOClient('http://localhost:' + port);
+		}
     }
 
 	sendParametersToBackend(params){
-        console.log("TEST")
+        console.log("Sending to backend... ")
         console.log(params)
 		this.socket.emit('parameterChange', params)
 	}
 
     dataListener(cb){
-        this.socket.on('data', data => cb(data))
+		if(this.useSampleListener){
+			sampleDataListener(cb);
+		}
+		else{
+			this.socket.on('data', data => cb(data))
+		}
 	}
 	
     sampleDataListener(cb){
 		// Creates sample data similar to arduino.
-		// Note/TODO: Change ranges to reflect real ranges or whatever you are testing.
-		let data_names = [ 
-			"checkSum",
-			"batteryPercentage",
-			"breathCompleted",
-			"tidalVolume", 
-			"errorCode",
-			"abnormalPressure", 
-			"abnormalFiO2"
-		]
-		let ranges = [
-			[0,0], // Check sum
-			[0,100], // Battery percent
-			[0,1], // Breath completed
-			[5,8], // Tidal volume
-			[0,0], // Error code
-			[0,0], // Abnormal pressure
-			[21,92] // Abnormal fio2
-		]
+		// Based on constants file, alarmMin and alarmMax.
 
 		// Interval 100 ms.
         setInterval(() => {
 			let data = []
-			for(let i = 0; i < data_names.length; i++){
-				data.push(Math.floor(Math.random() * (ranges[i][1] - ranges[i][0] + 1)) + ranges[i][0]);
+			for(const name of readingNames){
+				data.push(Math.floor(Math.random() * (readingsInfo[name].alarmMax - readingsInfo[name].alarmMin + 1)) + readingsInfo[name].alarmMin);
 			}
 			cb(data);
 		}, 100)
-
 	}
-	
-    // sampleTidalVolumeDataListener(cb){
-    //     setInterval(() => {
-	// 		let val = Math.floor(4*Math.random() + 10) + 1;
-	// 		cb({
-	// 			type: 'tidal volume',
-	// 			value: val
-	// 		})
-	// 	}, 1000)
-    // }
-
-    // samplePressureDataListener(cb) {
-    //     setInterval(() => {
-	// 		let val = Math.floor(3*Math.random() + 25 ) + 1;
-	// 		cb({
-	// 			type: 'pressure',
-	// 			value: val
-	// 		})
-	// 	}, 2000)
-    // }
     
 }
