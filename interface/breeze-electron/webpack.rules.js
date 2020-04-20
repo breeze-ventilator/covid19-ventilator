623bc601c013115d7772b2f3e7aa4a28a1d9d7b7
Raffi Hotter <rhotter@users.noreply.github.com>
2020-04-20 13:44:45 -0400

    working on mac maybe

--- interface/breeze-electron/webpack.rules.js
+++ interface/breeze-electron/webpack.rules.js
@@ -42,6 +42,17 @@ module.exports = [
       }
     ]
   }
+  // {
+  //   test: /\.py$/,
+  //   use: [
+  //     {
+  //       loader: 'file-loader',
+  //       options: {
+  //         name: '[name].[ext]',
+  //       }
+  //     }
+  //   ]
+  // }
   // Put your webpack loader rules in this array.  This is where you would put
   // your ts-loader configuration for instance:
   /**
