fb273ab56859e9d5c8a10f0edf9cc11eb429ba8a
Raffi Hotter <rhotter@users.noreply.github.com>
2020-04-20 15:43:25 -0400

    new code

--- firmware/arduino/arduino.ino
+++ firmware/arduino/arduino.ino
@@ -34,16 +34,16 @@ void setup() {
   // if (servosConnectedErrorCode != NO_ERROR) {
   //   piCommunication.sendServosNotConnectedErrorToPi(servosConnectedErrorCode);
   // }
-  // parameters.currentMode = PRESSURE_SUPPORT_MODE;
-  // parameters.currentFiO2 = 10;
-  // parameters.currentInspiratoryTime = 5000;
-  // parameters.currentMaxExpiratoryTime = 5000;
-  // parameters.currentInspiratoryPressure = 150; // mm H2O
-  // parameters.currentPEEP = 50; // mm H2O
-  // parameters.currentRiseTime = 100; // ms
-  // parameters.currentSensitivity = -1; // L
-  // parameters.currentApneaTime = 6000; // ms
-  // parameters.currentFlowCyclingOffPercentage = 0.20; // 20%
+  parameters.currentMode = PRESSURE_CONTROL_MODE;
+  parameters.currentFiO2 = 10;
+  parameters.currentInspiratoryTime = 5000;
+  parameters.currentMaxExpiratoryTime = 5000;
+  parameters.currentInspiratoryPressure = 200; // mm H2O
+  parameters.currentPEEP = 50; // mm H2O
+  parameters.currentRiseTime = 100; // ms
+  parameters.currentSensitivity = -1; // L
+  parameters.currentApneaTime = 6000; // ms
+  parameters.currentFlowCyclingOffPercentage = 0.20; // 20%
 }
 
 void loop() {
@@ -85,7 +85,7 @@ void loop() {
     data.resetTidalVolume();
   }
 
-  if (state.apneaTimeExceededError != NO_ERROR ) {
+  if (state.apneaTimeExceededError != NO_ERROR) {
     piCommunication.updateErrors(state);
     state.apneaTimeExceededError = NO_ERROR;
   }
--- firmware/arduino/src/Sensors/Sensors.cpp
+++ firmware/arduino/src/Sensors/Sensors.cpp
@@ -56,7 +56,7 @@ void Sensors::readSensorsIfAvailableAndSaveSensorData(Data &data, State &state) 
   // }
   if (isTimeToReadMainPressure()) {
     float pressureValue = mainPressureSensor.read(); // analog read (difference between this pressure and atmospheric pressure)
-    Serial.println(pressureValue);
+    // Serial.println(pressureValue);
     data.saveMainPressureReading(pressureValue);
     _lastMainPressureReadTime = millis();
   }
