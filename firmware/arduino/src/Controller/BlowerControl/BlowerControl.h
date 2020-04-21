fc42c899653c6731a92d258eaf3f68a0de45c255
Raffi Hotter <rhotter@users.noreply.github.com>
2020-04-21 13:43:56 -0400

    tune

--- firmware/arduino/src/Controller/BlowerControl/BlowerControl.h
+++ firmware/arduino/src/Controller/BlowerControl/BlowerControl.h
@@ -1,9 +1,10 @@
 #ifndef BLOWER_CONTROL_H
 #define BLOWER_CONTROL_H
 
-#define BLOWER_KI 0
-#define BLOWER_KP 0.75
-#define BLOWER_KD 0
+// Ku = 0.75, Tu = 0.23 sec
+#define BLOWER_KI 1.304
+#define BLOWER_KP 0.15 
+#define BLOWER_KD 0.0115
 #define BLOWER_FAN_SERVO_PIN 9
 #define BLOWER_PID_TIME 100
 
