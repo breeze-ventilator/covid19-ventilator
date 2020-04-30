0faba35108af80c4afe6705aeb978b13a15951ac
Raffi Hotter <rhotter@users.noreply.github.com>
2020-04-29 22:08:38 -0400

    no alarm when values are null

--- interface/breeze-electron/src/components/Alarms/AlarmsHelper.js
+++ interface/breeze-electron/src/components/Alarms/AlarmsHelper.js
@@ -1,18 +1,20 @@
 
 export function shouldAlarm(name, value, alarmRanges){
   // conditions for alarm notification
-  if (name == "abnormalPressure" && value < alarmRanges.pressure.min) { // workaround: abnormalPressure contains min
-    return "Pressure abnormally low"
-  }
-  if (name == "abnormalFiO2" && value > alarmRanges.pressure.max) {
-    return "Pressure abnormally high"
-  }
-  if (name == "Minute ventilation") {
-    if (value < alarmRanges[name].min) {
-      return "Minute ventilation low"
+  if (value != null) {
+    if (name == "abnormalPressure" && value < alarmRanges.pressure.min) { // workaround: abnormalPressure contains min
+      return "Pressure abnormally low"
     }
-    if (value > alarmRanges[name].max) {
-      return "Minute ventilation high"
+    if (name == "abnormalFiO2" && value > alarmRanges.pressure.max) {
+      return "Pressure abnormally high"
+    }
+    if (name == "Minute ventilation") {
+      if (value < alarmRanges[name].min) {
+        return "Minute ventilation low"
+      }
+      if (value > alarmRanges[name].max) {
+        return "Minute ventilation high"
+      }
     }
   }
   return ""
